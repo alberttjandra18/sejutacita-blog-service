b7f539f03587520d5c17141cbf5b34a0e693322b
Albert Tjandra <alberttjandra18@outlook.com>
2022-01-07 12:16:17 +0800

    Base init

---
+++ app.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+// use db
+require("./src/db/db");
+
+app.use((req, res, next) => {
+  console.log(`${new Date()} => ${req.originalUrl}`, req.body);
+  next();
+});
+
+//for Routes
+app.use([]);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Home" });
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("Lost?");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
